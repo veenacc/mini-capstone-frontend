3986ef0450e6e581e7c8ada746fe2b03b16bb2d0
veenacc <veena.chenicheri@gmail.com>
2024-06-03 21:28:15 -0400

    added cards styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,15 +1,35 @@
 import { ProductsIndex } from "./ProductsIndex";
+import  axios  from "axios";
+import { useState, useEffect } from "react";
 
+//call all functions inside of content
 export function Content() {
-  const products = [
-    {id:1, name:"apple", price: 10,description: "red delicious apples", is_discounted:"yes" ,supplier_id: 11, images: "https://www.applesfromny.com/wp-content/uploads/2020/05/20Ounce_NYAS-Apples2.png" },
-    {id:2, name:"green apple", price: 20,description: "green delicious apples", is_discounted:"no" ,supplier_id: 21, images: "https://t4.ftcdn.net/jpg/01/07/24/39/360_F_107243962_93O8mMEWlLU1mGgEdwS0dIv37fbLWfaV.jpg" }
-     ];
-     console.log(products);
+
+  const [products, setProducts] = useState([]);
+
+  const handleProductsIndex = ()=>{
+    console.log("can i see all products");
+    axios.get("http://localhost:3000/products.json").then (
+      response => {
+        console.log(response.data);
+        console.log("I'm in function handleAllposts");
+        setProducts(response.data);
+      }
+    )
+    
+  }
+  // function call :calls function handleProductIndex and sets value of "products"
+    useEffect(handleProductsIndex, []) ;
+  /* ************************************************************** */
+  // const products = [
+    // {id:1, name:"apple", price: 10,description: "red delicious apples", is_discounted:"yes" ,supplier_id: 11, images: "https://www.applesfromny.com/wp-content/uploads/2020/05/20Ounce_NYAS-Apples2.png" },
+    // {id:2, name:"green apple", price: 20,description: "green delicious apples", is_discounted:"no" ,supplier_id: 21, images: "https://t4.ftcdn.net/jpg/01/07/24/39/360_F_107243962_93O8mMEWlLU1mGgEdwS0dIv37fbLWfaV.jpg" }
+    //  ];
+    //  console.log(products);
   return (
     <main>
   
-      <div><ProductsIndex  products={products}/></div>
+      <div><ProductsIndex  products= {products} /></div>
       
     </main>
   );
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -6,18 +6,26 @@ export function ProductsIndex(props)
     // <div>console.log(props.products)</div>
     <div>
       <h1>All Products</h1>
+      <div className="cards">
       {props.products.map((product) => (
-        <div key={product.id}>
+        <div key={product.id}className="products cards" >
           <p> id : {product.id}</p>
           <p>name: {product.name}</p>
           <p>price: {product.price}</p>
           <p>description: {product.description}</p>
           <p>is_discounted: {product.is_discounted}</p>
-          <p> supplier_id: {product.supplier_id}</p>
-          <img src= {product.images} height="300" width="300" /> <hr />
+          <p>total: {product.total}</p>
+          <p>item_count: {product.item_count}</p>
+          <p> supplier_id: {product.supplier_id}</p> <br />
+          <img src= {product.images[0] ? product.images[0].url :
+            "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"} alt= ""
+             /> <hr />
         </div>
       ))}
+      </div>
     </div>
     
     );
 }
+
+{/* <img src={product.images[0] ? product.images[0].url : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"} alt="" /> */}
